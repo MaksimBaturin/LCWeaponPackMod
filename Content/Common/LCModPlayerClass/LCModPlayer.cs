276b1acf3702ea35fb5b86794d85650ef0b60ccd
MaksimBaturin <baturbaya@yandex.ru>
2024-11-21 19:30:11 +0300

    feat: Added simplified sanity system

---
+++ Content/Common/LCModPlayerClass/LCModPlayer.cs
@@ -0,0 +1,28 @@
+﻿using LCWeaponPack.Content.Common.Systems;
+using LCWeaponPack.Content.Items;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace LCWeaponPack.Content.Common.LCModPlayerClass
+{
+    internal class LCModPlayer : ModPlayer
+    {
+        private SanitySystem sanitySystem = ModContent.GetInstance<SanitySystem>();
+        public override void OnHurt(Player.HurtInfo info)
+        {
+            sanitySystem.DecreaseSanity(info.Damage);
+            base.OnHurt(info);
+        }
+        public override void OnRespawn()
+        {
+            sanitySystem.CurrentSanity = 0;
+            base.OnRespawn();
+        }
+
+        public override void OnHitNPCWithItem(Item item, NPC target, NPC.HitInfo hit, int damageDone)
+        {
+            sanitySystem.IncreaseSanity(damageDone);
+            base.OnHitNPCWithItem(item, target, hit, damageDone);
+        }
+    }
+}
---
+++ Content/Common/Systems/SanitySystem.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+using Terraria;
+using LCWeaponPack.Content.Common.LCModPlayerClass;
+
+namespace LCWeaponPack.Content.Common.Systems
+{
+    internal class SanitySystem : ModSystem
+    {
+        private Player player;
+        public int CurrentSanity { get; set; } = 0;
+
+        public override void OnWorldLoad()
+        {
+            player = Main.LocalPlayer;
+        }
+        public void displayName()
+        {
+            Main.NewText(player.name, 150, 250, 150);
+        }
+
+        public void DecreaseSanity(int hurtDamage)
+        {
+            Main.NewText("HurtDamage: "+ hurtDamage.ToString(), 50, 250, 150);
+
+            int newSanity = CurrentSanity - 5 ;
+            if (newSanity < -45) CurrentSanity = -45;
+            else CurrentSanity = newSanity;
+
+
+            Main.NewText("Sanity: " + CurrentSanity.ToString(), 50, 250, 150);
+        }
+
+        public void IncreaseSanity(int doneDamage)
+        {
+            Main.NewText("DoneDamage: " + doneDamage.ToString(), 50, 250, 150);
+
+            int newSanity = CurrentSanity + 5;
+            if (newSanity > 45) CurrentSanity = 45;
+            else CurrentSanity = newSanity;
+
+            Main.NewText("Sanity: " + CurrentSanity.ToString(), 50, 250, 150);
+        }
+    }
+}
