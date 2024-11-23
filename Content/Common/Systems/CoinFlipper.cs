9c7859e44cf1a0a81b93864bb678f596624a292d
MaksimBaturin <baturbaya@yandex.ru>
2024-11-23 21:28:40 +0300

    feat: Add coin chance depends on sanity

--- Content/Common/Systems/CoinFlipper.cs
+++ Content/Common/Systems/CoinFlipper.cs
@@ -3,15 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Terraria.ModLoader;
 
 namespace LCWeaponPack.Content.Common.Systems
 {
-    internal static class CoinFlipper
+    internal class CoinFlipper
     {
-        public static bool MakeCoinFlip()
+        private SanitySystem sanitySystem = ModContent.GetInstance<SanitySystem>();
+
+        private int headChance = 50;
+        public bool MakeCoinFlip()
         {
             Random random = new Random();
-            if (random.Next(0,1) == 1)
+
+            headChance = 50 + sanitySystem.CurrentSanity;
+
+            if (random.Next(0, headChance+1) <= headChance)
             {
                 return true;
             }
